20be7a42201742e4d284b951090459fbb5cac0ab
Hexandcube <hexandcube@hexandcube.eu.org>
2024-04-21 15:06:22 +0200

    Initial Commit

---
+++ app/src/main/java/org/eu/hexandcube/pwa/DelegationService.java
@@ -0,0 +1,14 @@
+package org.eu.hexandcube.pwa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
